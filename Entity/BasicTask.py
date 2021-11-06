b007ee494140b3d534298a0d2d5fa649ae132db3
andrewp-li <80422758+andrewp-li@users.noreply.github.com>
2021-11-06 13:19:54 -0400

    basic task

--- Entity/BasicTask.py
+++ Entity/BasicTask.py
@@ -8,10 +8,5 @@ class BasicTask(Task):
     def __init__(self):
         super().__init__()
 
-    def completed(self) -> bool:
-        self.completed = True
-
     def is_completed(self) -> bool:
         return self.completed
-
-    def get_due_date(self) -> None:
