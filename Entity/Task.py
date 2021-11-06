ec4ea36fcdb27f7c89cf289fca4b6f2965970822
osakana373 <80421190+0sakana373@users.noreply.github.com>
2021-11-06 13:01:08 -0400

    Update Task.py

--- Entity/Task.py
+++ Entity/Task.py
@@ -3,14 +3,27 @@ class Task:
     Abstract Class
     """
 
-    def __init__(self, title, notes = "", due_date = None):
+    def __init__(self, title, notes="", due_date=None):
         self.title = title
         self.notes = notes
         self.complete = False
         self.due_date = due_date
 
-    def complete(self) -> bool:
-        pass
+    def completed(self) -> None:
+        """
+        abstract method
+        Mutate self.complete from false to true
+        :return: None
+        """
+        self.complete = True
+
+    def uncompleted(self) -> None:
+        """
+        abstract method
+        Mutate self.complete from true to false
+        :return:
+        """
+        self.complete = False
 
     # make sure you throw appropriate exceptions
     def get_title(self) -> str:
@@ -19,28 +32,28 @@ class Task:
         Returns name of task title
         :return: title as string. If no title return ""
         """
-        pass
+        return self.title
 
     def is_completed(self) -> bool:
         """
         Abstract method
         :return: True or false based on whether task is completed
         """
-        pass
+        return self.complete
 
     def get_notes(self) -> str:
         """
         Abstract method
         :return: notes for the task as a string. If no notes return ""
         """
-        pass
+        return self.notes
 
     def get_due_date(self) -> None:
         """
         Abstract method
         :return: a date. THINK OF THIS LATER
         """
-        pass
+        return self.due_date
 
     def add_due_date(self) -> None:
         """
@@ -58,12 +71,12 @@ class Task:
         """
         pass
 
-    def change_title(self) -> None:
+    def change_title(self, newtitle) -> None:
         """
         Abstact method
         Mutates title
         :return: None
         """
-        pass
+        self.title = newtitle
 
     pass
