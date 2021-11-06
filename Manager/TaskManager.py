e3b62fb481d92aaa722a8e0aad8ffa3734a2529b
osakana373 <80421190+0sakana373@users.noreply.github.com>
2021-11-06 14:58:33 -0400

    taskmanager start

--- Manager/TaskManager.py
+++ Manager/TaskManager.py
@@ -1,7 +1,11 @@
+<<<<<<< Updated upstream
 from Manager.Gettable import Gettable
+=======
+from Manager.Derived import Gettable, Saveable
+>>>>>>> Stashed changes
 
 
-class TaskManager(Gettable):
+class TaskManager(Gettable, Saveable):
     """
     Manages use cases for a BigTask.
     What needs to be done?
@@ -12,6 +16,18 @@ class TaskManager(Gettable):
     - giving up data
     - keeps track of due date.
     """
+    def __init__(self, type, title="", notes="", due_date=None):
+        if self.type is ProjectTask:
+            self.maintask = ProjectTask(title, notes, due_date)
+        if self.type is DailyTask:
+            self.maintask = DailyTask(title, notes, due_date)
+
+    def add_subtask(self):
+
+    def remove_subtask(self, task_id):
+        
+    def get_subtask(self, task_id):
+
     def complete_subtask(self, task_id) -> None:
         """
         Completes a subtask. You are able to select subtask by id
